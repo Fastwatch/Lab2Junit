c11ed889068040eae6bbeadde49d54d3bec2ef80
Riley Mahr <rwmahr@gmail.com>
2018-01-30 12:26:24 -0600

    adding classes

---
+++ src/Scoresheet.java
@@ -0,0 +1,20 @@
+
+public class Scoresheet {
+	private Frame[] frames = new Frame[10];
+	
+	public int getFrameScore(int frameNumber) {
+		return 0;
+	}
+	
+	public int getGameScore() {
+		return 0;
+	}
+	
+	public void bowl(int pins) {
+		
+	}
+	
+	public void newGame() {
+		
+	}
+}
