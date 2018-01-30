55debc7c100ae926e484e0e4a16fc68381f15e09
Riley Mahr <rwmahr@gmail.com>
2018-01-30 13:35:10 -0600

    classes completed... we think

--- src/Frame.java
+++ src/Frame.java
@@ -1,4 +1,38 @@
 
 public class Frame {
+	private int score, bowls;
+	
+	public Frame() {
+		score = bowls = 0;
+	}
 
+	public int getScore() {
+		return score;
+	}
+	
+	public void addToScore(int add) {
+		if(add < 0 || add > 10) throw new IllegalArgumentException("Illegal Score to add");
+		score += add;
+	}
+	
+	public void bowl(int pins) {
+		if(pins < 0 || (pins + score) > 10) {
+			throw new IllegalArgumentException("illegal number of pins");
+		}
+		if(bowls > 1) {
+			throw new IllegalStateException("Can't bowl more on this frame");
+		}
+		++bowls;
+		score += pins;
+		
+	}
+	
+	public boolean canBowl() {
+		return (bowls < 2 || score < 10);
+	}
+	
+	public boolean isStrike() {
+		return (bowls == 1 && score == 10);
+	}
+	
 }
--- src/Scoresheet.java
+++ src/Scoresheet.java
@@ -1,20 +1,75 @@
 
 public class Scoresheet {
-	private Frame[] frames = new Frame[10];
+	private Frame[] frames;
+	private int currentFrame;
+	private Frame add2;
+	private Frame[] add1 = new Frame[2];
+	
+	public int getCurrentFrame() {
+		return currentFrame;
+	}
 	
 	public int getFrameScore(int frameNumber) {
-		return 0;
+		return frames[frameNumber].getScore();
 	}
 	
 	public int getGameScore() {
-		return 0;
+		int total = 0;
+		for (Frame f : frames) {
+			total += f.getScore();
+		}
+		return total;
 	}
 	
 	public void bowl(int pins) {
 		
+		if(!canPlay()) throw new IllegalStateException("Cannot Play, the game is finished");
+		
+		frames[currentFrame].bowl(pins);
+		
+		if(!frames[currentFrame].canBowl()) {
+			
+			updateScores();
+			
+			if(frames[currentFrame].isStrike()) {
+				add2 = frames[currentFrame];
+			} else if(frames[currentFrame].getScore() == 10) {
+				add1[1] = frames[currentFrame];
+			}
+			
+			++currentFrame;
+		}
 	}
 	
 	public void newGame() {
+		for (Frame f : frames) {
+			f = new Frame();
+		}
+		currentFrame = 0;
 		
 	}
+	
+	private boolean canPlay() {
+		if(currentFrame < 10)
+			return true;
+		return false;
+	}
+	
+	private void updateScores() {
+		
+		int score = frames[currentFrame].getScore();
+		
+		if(add2 != null)
+			add2.addToScore(score);
+		for (Frame f : add1) {
+			if(f != null)
+				f.addToScore(score);
+		}
+		
+		add1 = new Frame[2];
+		add1[0] = add2;
+		add2 = null;
+	}
+	
+	
 }
